ae603dbbf33355a87579b9823218c76055d9f74e
elmorisor <florian-adamus@gmx.de>
2018-03-23 08:27:55 +0100

    MUAHAHAHAHAHA

--- src/Supervisor.java
+++ src/Supervisor.java
@@ -3,23 +3,24 @@ import java.util.logging.*;
 
 public class Supervisor {
 
-	private ArrayList<Processor> processors;
+  private ArrayList<Processor> processors;
     private ArrayList<Process> finishedProcesses;
     private Dispatcher dispatcher;
+    //HAHAHAHAHAHAHAHAHAHA
 
     Supervisor(){
-    	this.processors = new ArrayList<Processor>();
-    	this.finishedProcesses = new ArrayList<Process>();
-    	}
-	public void setProcessors(ArrayList<Processor> processors) {
-		this.processors = processors;
-	}
-	public void setDispatcher(Dispatcher dispatcher) {
-		this.dispatcher = dispatcher;
-	}
+      this.processors = new ArrayList<Processor>();
+      this.finishedProcesses = new ArrayList<Process>();
+      }
+  public void setProcessors(ArrayList<Processor> processors) {
+    this.processors = processors;
+  }
+  public void setDispatcher(Dispatcher dispatcher) {
+    this.dispatcher = dispatcher;
+  }
 public void log() {
   }
   public void handoverProcess(Process p) {
-	finishedProcesses.add(p);  
+  finishedProcesses.add(p);  
   }
 }
